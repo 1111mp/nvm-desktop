5a5ee1d2c24468db4d495e4a85e970006355138d
The1111mp <The1111mp@outlook.com>
2024-02-02 20:54:48 +0800

    upgrade nvm-desktop version v2.6.2 -> v3.0.0
    Signed-off-by: The1111mp <The1111mp@outlook.com>

--- src/main/utils/migration.ts
+++ src/main/utils/migration.ts
@@ -14,7 +14,7 @@ import { app } from 'electron';
 
 import { APPDIR, BIN_DIR, MIRRATION_FILE } from '../constants';
 
-const CURRENT_MIGRATION_VERSION: number = 8;
+const CURRENT_MIGRATION_VERSION: number = 9;
 
 export async function updateSchema() {
   const schemaVersion = await getSchemaVersion();

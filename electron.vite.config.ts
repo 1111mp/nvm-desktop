e8b64ac1a3ec0fbcedae0346a15f4b4bafca2301
The1111mp <The1111mp@outlook.com>
2024-03-02 20:16:18 +0800

    fix: upgrade electron-vite-tsup version v2.0.3 -> v2.0.4 (parse runtime args)
    Signed-off-by: The1111mp <The1111mp@outlook.com>

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -16,15 +16,13 @@ export default defineConfig(({ mode }) => {
       format: "esm",
       // The requested module 'electron-updater' is a CommonJS module, which may not support all module.exports as named exports.
       noExternal: isProd ? [/(.*)/] : ["electron-updater"],
-      minify: isProd,
-      watch: !isProd
+      minify: isProd
     },
     preload: {
       clean: true,
       entry: { preload: isTest ? "src/preload/preload.test.ts" : "src/preload/index.ts" },
       format: "cjs",
-      minify: isProd,
-      watch: !isProd
+      minify: isProd
     },
     renderer: {
       resolve: {

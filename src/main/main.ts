b3bbfd6b0fc9442083164e494eda577712905361
The1111mp <The1111mp@outlook.com>
2024-07-28 18:30:32 +0800

    fix: launching multiple instances (https://github.com/1111mp/nvm-desktop/issues/101)
    Signed-off-by: The1111mp <The1111mp@outlook.com>

--- src/main/main.ts
+++ src/main/main.ts
@@ -202,7 +202,7 @@ app
       locale = loadLocale({ appLocale });
     }
 
-    createWindow(code);
+    mainWindow === null && createWindow(code);
     app.on("activate", () => {
       // On macOS it's common to re-create a window in the app when the
       // dock icon is clicked and there are no other windows open.

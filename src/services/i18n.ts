41757ac1e280d93a6bee7c5cbe275d8bb3b1284a
nxen <nxen816@gmail.com>
2026-05-21 04:48:54 +0200

    Add Polish translation

--- src/services/i18n.ts
+++ src/services/i18n.ts
@@ -2,11 +2,15 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import en from '@/locales/en.json';
+import pl from '@/locales/pl.json';
 import zh_CN from '@/locales/zh_CN.json';
 
 const resources = {
   en: {
     translation: en,
+  },
+  pl: {
+    translation: pl,
   },
   'zh-CN': {
     translation: zh_CN,
